0a918e6605e65b4867c8e1c375c3bcb7dfd89644
jspaulding42 <jspaulding42@gmail.com>
2013-02-13 18:34:51 -0700

    pushing a test change

--- src/com/alscodev/ectocraft/MainActivity.java
+++ src/com/alscodev/ectocraft/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //this is a test change;
     }
 
     @Override
